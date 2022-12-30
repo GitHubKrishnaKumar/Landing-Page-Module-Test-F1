c7e5e728fc5caa8f109f8649e400b70d05901488
GitHubKrishnaKumar <kksoni1432@gmail.com>
2022-12-30 21:36:40 +0530

    done

---
+++ script.js
@@ -0,0 +1,11 @@
+function formData(){
+    var fn = document.getElementById('fn');
+    var ln = document.getElementById('ln');
+    var em = document.getElementById('em');
+
+    if (fn.value === '' | ln.value==='' | em.value===''){
+        
+    }else {
+        alert("Registration Done...");
+    }
+}
